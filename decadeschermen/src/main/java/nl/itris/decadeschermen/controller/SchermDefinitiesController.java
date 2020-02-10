3f2e87dabab8033e51719b9561fbd3efbce01f24
roelapeldoorn <roel.apeldoorn@falkenapark23.nl>
2020-02-10 04:22:40 +0100

    Zoeken stap 1

--- decadeschermen/src/main/java/nl/itris/decadeschermen/controller/SchermDefinitiesController.java
+++ decadeschermen/src/main/java/nl/itris/decadeschermen/controller/SchermDefinitiesController.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import nl.itris.decadeschermen.config.OracleJdbcTemplateBuilder;
 import nl.itris.decadeschermen.mysql.domain.DecadeEnvironment;
 import nl.itris.decadeschermen.mysql.repository.EnvironmentRepository;
+import nl.itris.decadeschermen.oracle.domain.DecadeSchermDefinitie;
 import nl.itris.decadeschermen.oracle.domain.DecadeSchermDefinitieDao;
 import nl.itris.decadeschermen.oracle.domain.ViewpointOrganizationDao;
 
@@ -20,16 +22,18 @@ import nl.itris.decadeschermen.oracle.domain.ViewpointOrganizationDao;
 public class SchermDefinitiesController {
 
 	private final EnvironmentRepository environmentRepository;
+	private String zoekSchermDefinitie;
     
     DecadeEnvironment decadeEnvironment = new DecadeEnvironment();
     
     @Autowired
     public SchermDefinitiesController(EnvironmentRepository environmentRepository) {
     	this.environmentRepository = environmentRepository;
+    	this.zoekSchermDefinitie = "";
     }
 
     @GetMapping("/{environmentid}")
-    public String showModules(@PathVariable("environmentid") long environmentid, Model model) {
+    public String showSchermDefinities(@PathVariable("environmentid") long environmentid, Model model) {
 
     	this.decadeEnvironment = environmentRepository.findById(environmentid)
     			.orElseThrow(() -> new IllegalArgumentException("Omgeving met ID: " + environmentid + " niet gevonden!"));
@@ -43,13 +47,26 @@ public class SchermDefinitiesController {
 
     	DecadeSchermDefinitieDao decadeSchermDefinitieDao = new DecadeSchermDefinitieDao();
     	decadeSchermDefinitieDao.setJdbcTemplate(oracleJdbcTemplateBuilder.getJdbcTemplate(this.decadeEnvironment));
-    	model.addAttribute("schermdefinities", decadeSchermDefinitieDao.findAllSchermDefinities());
+    	model.addAttribute("schermdefinities", decadeSchermDefinitieDao.findAllSchermDefinities(this.zoekSchermDefinitie));
  
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     	model.addAttribute("authenticated", authentication);
-           	
+
+        model.addAttribute("zoekschermdefinitie", new DecadeSchermDefinitie());
+
         return "schermdefinities-index";
     	
+    }
+    
+
+    @PostMapping("/Zoek")
+    public String lectureSearch(DecadeSchermDefinitie zoekDecadeSchermDefinitie, Model model) {
+
+    	this.zoekSchermDefinitie = zoekDecadeSchermDefinitie.getStpdverkortenaam();
+    	System.out.println("Zoekterm " + this.zoekSchermDefinitie);
+
+        return "schermdefinities-index";
+        
     }
 
 }
--- decadeschermen/src/main/java/nl/itris/decadeschermen/oracle/domain/DecadeSchermDefinitieDao.java
+++ decadeschermen/src/main/java/nl/itris/decadeschermen/oracle/domain/DecadeSchermDefinitieDao.java
@@ -26,9 +26,13 @@ public class DecadeSchermDefinitieDao {
         
     }
 
-    public List<DecadeSchermDefinitie> findAllSchermDefinities() {
+    public List<DecadeSchermDefinitie> findAllSchermDefinities(String zoekSchermDefinitie) {
     
-    	String SQL = "SELECT DISTINCT STPD_VERKORTE_NAAM FROM DF_STAND_SCHERM_DEFINITIES WHERE STPD_DSSM_CODE = 'DF' ORDER BY STPD_VERKORTE_NAAM";
+    	String SQL = "SELECT DISTINCT STPD_VERKORTE_NAAM FROM DF_STAND_SCHERM_DEFINITIES WHERE STPD_DSSM_CODE = 'DF'";
+    	if (!zoekSchermDefinitie.equals("")) {
+    		SQL = SQL + " AND STPD_VERKORTE_NAAM LIKE '" + zoekSchermDefinitie + "%";
+    	}
+    	SQL = SQL + " ORDER BY STPD_VERKORTE_NAAM";
     	
     	List<DecadeSchermDefinitie> decadeSchermDefinities = this.jdbcTemplateObject.query(SQL, new DecadeSchermDefinitieMapper());
     	
