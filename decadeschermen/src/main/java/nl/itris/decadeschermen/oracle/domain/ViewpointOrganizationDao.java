348edca054b3d7934c9b90bf6634b696164dfd3d
roel <roel@imac-van-roel.itnw.itris.lan>
2020-03-06 14:54:46 +0100

    Nog eentje

--- decadeschermen/src/main/java/nl/itris/decadeschermen/oracle/domain/ViewpointOrganizationDao.java
+++ decadeschermen/src/main/java/nl/itris/decadeschermen/oracle/domain/ViewpointOrganizationDao.java
@@ -1,5 +1,8 @@
 package nl.itris.decadeschermen.oracle.domain;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Component;
 
 import nl.itris.decadeschermen.mysql.domain.DecadeEnvironment;
@@ -9,9 +12,24 @@ public class ViewpointOrganizationDao extends DefaultDao {
 
     public ViewpointOrganization findByRosid(DecadeEnvironment decadeEnvironment) {
 
-    	String SQL = "SELECT ROS_ID, ROS_ORGANIZATION_NAME FROM V_RED_ORGANIZATIONS WHERE ROWNUM = 1";
-        
-    	ViewpointOrganization viewpointOrganization = this.jdbcTemplateObject.queryForObject(SQL, new ViewpointOrganizationMapper());
+    	ViewpointOrganization viewpointOrganization = new ViewpointOrganization();
+    	
+    	try {
+    		
+    		Connection connection = getConnection(decadeEnvironment);
+    		
+    		String SQL = "SELECT ROS_ID, ROS_ORGANIZATION_NAME FROM V_RED_ORGANIZATIONS WHERE ROWNUM = 1";
+            
+        	ViewpointOrganization viewpointOrganization = this.jdbcTemplateObject.queryForObject(SQL, new ViewpointOrganizationMapper());
+
+    		
+    		
+    		
+    	} catch(SQLException sqlException) {
+    		
+    	}
+    	
+    	
 
     	return viewpointOrganization;
         
